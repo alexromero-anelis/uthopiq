89ba21fec2ada25db046ec8fade87364175b0127
ImTommyDev <tommy.devlpr@gmail.com>
2025-09-18 11:42:04 +0200

    #13 - Se ha eliminado la pagina de personalizar plan

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import PlanesWeb from './components/Planes/Planes.jsx';
 import Automatizaciones from './components/Planes/PlanesAutomatizacion.jsx';
 import Contacto from './components/Contacto/Contacto.jsx';
 import QuienesSomos from './components/QuienesSomos/QuienesSomos.jsx';
-import PersonalizaPlan from './pages/PersonalizaPlan.jsx';
 import PaginaProyectos from './pages/ProyectosPage.jsx';
 // import ChatWidgetLoader from './components/ChatBot/ChatWidgetLoader.jsx'
 import ComoLoHacemos from "./components/ComoLoHacemos/ComoLoHacemos.jsx";
@@ -54,7 +53,6 @@ function App() {
             </>
           }
         />
-        <Route path="/personaliza-tu-plan" element={<PersonalizaPlan />} />
         <Route path="/proyectos" element={<PaginaProyectos />} />
       </Routes>
     </Router>
