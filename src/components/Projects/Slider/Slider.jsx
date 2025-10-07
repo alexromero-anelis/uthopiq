62a44df10667d80522229a4a359f9c9532b18c1d
alexromero-anelis <a.romero@anelis.com>
2025-10-07 17:12:31 +0200

    Mejora del desplazamiento del slider y añadir flechas

--- src/components/Projects/Slider/Slider.jsx
+++ src/components/Projects/Slider/Slider.jsx
@@ -1,27 +1,40 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import './slider.css'
-import { Autoplay } from 'swiper/modules';
+import './slider.css';
+import { Autoplay, Navigation, FreeMode } from 'swiper/modules';
 import CardProjects from '../CardProjects/CardProjects';
 
 function Slider({ projects }) {
-    return (
-        <Swiper
-            modules={[Autoplay]}
-            autoplay={{ delay: 3000 }}
-            loop={true}
-            spaceBetween={60}
-            slidesPerView="auto"
-            speed={3000}
-            >
-            {projects.map((project, index) => (
-                <SwiperSlide key={index} style={{ width: '340px' }}>
-                    <CardProjects {...project} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+  return (
+    <div className="slider-wrap">
+      <Swiper
+        modules={[Autoplay, Navigation, FreeMode]}
+        rewind={true}
+        navigation={true}
+        autoplay={{ delay: 3500, disableOnInteraction: false, pauseOnMouseEnter: true }}
+        spaceBetween={60}
+        slidesPerView={'auto'}
+        speed={600}
+        freeMode={{ enabled: true, momentum: true, momentumRatio: 0.25, momentumVelocityRatio: 0.5, sticky: true }}
+        grabCursor={true}
+        touchRatio={0.55}
+        longSwipesMs={120}
+        longSwipesRatio={0.3}
+        resistance={true}
+        resistanceRatio={0.85}
+        centerInsufficientSlides={true}
+        watchOverflow={true}
+      >
+        {projects.map((project, index) => (
+          <SwiperSlide key={index} style={{ width: '340px' }}>
+            <CardProjects {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 export default Slider;
